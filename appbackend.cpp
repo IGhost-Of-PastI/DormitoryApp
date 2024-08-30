e257e6184e6fb058c6bcfd543a9675e4a323ecbd
IGhost-Of-PastI <tbordovich@gmail.com>
2024-08-30 15:32:50 +0300

    Добавлен класс бэкэнд прилоежния

--- appbackend.cpp
+++ appbackend.cpp
@@ -2,4 +2,11 @@
 
 appbackend::appbackend(QObject *parent)
     : QObject{parent}
-{}
+{
+    m_connection= QSqlDatabase::addDatabase("QPSQL");
+    m_connection.setHostName("localhost");
+    m_connection.setPort(5432);
+    m_connection.setUserName("postgres");
+    m_connection.setPassword("masterkey");
+    //m_connection.setDatabaseName();
+}
--- appbackend.h
+++ appbackend.h
@@ -1,14 +1,25 @@
 #pragma once
 
 #include <QObject>
+#include <QQmlEngine>
+#include <QSqlDatabase>
 
 class appbackend : public QObject
 {
     Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+
+    Q_PROPERTY(QSqlDatabase connection READ connection CONSTANT)
 public:
     explicit appbackend(QObject *parent = nullptr);
+    //~appbackend();
 
-signals:
+    QSqlDatabase connection() const {
+        return m_connection;
+    }
+private:
+    QSqlDatabase m_connection;
 };
 
 //#endif // APPBACKEND_H
