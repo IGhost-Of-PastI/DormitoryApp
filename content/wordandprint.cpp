4d4979a8a0038b5f5eaffd756e3d749d711b1ac6
IGhost-Of-PastI <95056441+IGhost-Of-PastI@users.noreply.github.com>
2024-09-22 16:00:01 +0300

    Сделана страница отчётов

--- content/wordandprint.cpp
+++ content/wordandprint.cpp
@@ -1,5 +1,53 @@
 #include "wordandprint.h"
+#include <windows.h>
+#include<commdlg.h>
 
 WordAndPrint::WordAndPrint(QObject *parent)
     : QObject{parent}
 {}
+
+bool WordAndPrint::importInWord(QString tablename)
+{
+    QSqlQuery query(SQLConnectionMenager::getConnection());
+    query.prepare(QString(R"(Select * from %1)").arg(tablename));
+   // query.bindValue(":tablename",tablename);
+    if (!query.exec()) {
+        qDebug()<<query.lastQuery();
+        qWarning() << "Query execution failed: " << query.lastError().text();
+    //    return;
+    }
+
+    QAxObject *wordApp = new QAxObject("Word.Application");
+    wordApp->dynamicCall("SetVisible(bool Visible)", true);
+    QAxObject *documents = wordApp->querySubObject("Documents");
+    documents->dynamicCall("Add()");
+    QAxObject *document = wordApp->querySubObject("ActiveDocument");
+
+    QAxObject *range = document->querySubObject("Range()");
+    QAxObject *table = document->querySubObject("Tables")->querySubObject("Add(Range, NumRows, NumColumns)", range->asVariant(), query.size() + 1, query.record().count());
+
+    // Set table headers
+    for (int col = 0; col < query.record().count(); ++col) {
+        QAxObject *cell = table->querySubObject("Cell(Row, Column)", 1, col + 1);
+        cell->querySubObject("Range")->dynamicCall("SetText(QString)", query.record().fieldName(col));
+    }
+
+    // Fill table with data
+    int row = 2;
+    while (query.next()) {
+        for (int col = 0; col < query.record().count(); ++col) {
+            QAxObject *cell = table->querySubObject("Cell(Row, Column)", row, col + 1);
+            cell->querySubObject("Range")->dynamicCall("SetText(QString)", query.value(col).toString());
+        }
+        ++row;
+    }
+
+    QString programDir = QCoreApplication::applicationDirPath();
+    QString filePath = programDir + "/document.docx";
+
+    document->dynamicCall("SaveAs(const QString&)", filePath);
+    wordApp->dynamicCall("Quit()");
+    return false;
+}
+
+
--- content/wordandprint.h
+++ content/wordandprint.h
@@ -3,14 +3,23 @@
 
 #include <QObject>
 #include <QQmlEngine>
+#include <QAxObject>
+#include <QTableView>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QCoreApplication>
+#include "mainsqlconnection.h"
 
 class WordAndPrint : public QObject
 {
     Q_OBJECT
+    QML_SINGLETON
     QML_ELEMENT
 public:
     explicit WordAndPrint(QObject *parent = nullptr);
-
+    Q_INVOKABLE bool importInWord(QString tablename);
+    //Q_INVOKABLE bool printDoc();
 signals:
 };
 
